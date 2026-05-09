9636395ea6dfeec16c169a3c437850584d0ce051
TheRudyPlayer <elerandomxd25@outlook.com>
2026-05-09 19:26:49 -0400

    Add multiple GUILD_IDs for the bot

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const http = require('http');
 const TOKEN = process.env.TOKEN;
 
 const CLIENT_ID = '1498803742391406633';
-const GUILD_ID = '1433246929588060432';
+const GUILD_ID = '1433246929588060432','1501669636700373002';
 
 // CLIENTE
 const client = new Client({
